e92ec4795f3971b31a1d0c2ae67d6bffd8d03b6c
Shota Inoue <925shota@gmail.com>
2020-05-30 12:44:08 +0900

    added views

--- app.js
+++ app.js
@@ -1,24 +1,11 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
-const mongoose = require('mongoose');
 const flash = require('connect-flash');
 const session = require('express-session');
-const passport = require('passport');
 
 require('dotenv').config();
 
 const app = express();
-
-// Passport config
-require('./config/passport')(passport);
-
-// DB config
-const db = require('./config/keys').MongoURI;
-
-// Connect to Mongo
-mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected...'))
-    .catch(err => console.log(err));
 
 //EJS
 app.use(expressLayouts);
@@ -33,10 +20,6 @@ app.use(session({
     resave: true,
     saveUninitialized: true,
   }));
-
-// Passport middleware
-app.use(passport.initialize());
-app.use(passport.session());
 
 // Connect flash
 app.use(flash());
